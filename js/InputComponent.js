c2d6a7fde2adcb99ceba655b876de67653890397
Kevin Grayson <k.grayson.ms@gmail.com>
2018-05-21 21:18:51 -0700

    Added input text box component and render

---
+++ js/InputComponent.js
@@ -0,0 +1,19 @@
+'use strict';
+/* globals */
+/* exported InputText */
+
+const inputTemplate = document.getElementById('input-template').content;
+
+class InputText {
+
+    constructor() {
+
+    }
+
+    render() {
+
+        const dom = inputTemplate;
+
+        return dom;
+    }
+}
