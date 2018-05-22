00d2490d63652714b1a2fc387f85903565b487c4
Kevin Grayson <k.grayson.ms@gmail.com>
2018-05-22 10:35:23 -0700

    Added CSS options and text box input, wired user input to live display

--- js/InputComponent.js
+++ js/InputComponent.js
@@ -1,6 +1,6 @@
 'use strict';
 /* globals */
-/* exported InputText */
+/* exported InputText updateInputWord */
 
 const inputTemplate = document.getElementById('input-template').content;
 
@@ -14,6 +14,18 @@ class InputText {
 
         const dom = inputTemplate;
 
+        
+
         return dom;
+    }
+}
+
+function updateInputWord() {
+    const input = document.getElementById('input-text-box').value;
+    const displayWord = document.getElementById('displayWord');
+    if(input) {
+        displayWord.innerHTML = input;
+    } else {
+        displayWord.innerHTML = 'Enter text!';
     }
 }
